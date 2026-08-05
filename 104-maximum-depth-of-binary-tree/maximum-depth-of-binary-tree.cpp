90e6f0ddb5e3a01129e718b9721905ac0b867ad9
Divyam Choudhary <divyamchoudhary15@gmail.com>
2026-08-05 14:32:11 +0530

    Time: 0 ms (100.00%) | Memory: 22.3 MB (16.15%) - LeetSync

--- 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.cpp
+++ 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.cpp
@@ -6,36 +6,21 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
- * right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
+private:
+    int f(TreeNode* root){
+        if(!root) return 0;
+
+        int l = f(root->left);
+        int r = f(root->right);
+
+        return 1 + max(l,r);
+    }
 public:
     int maxDepth(TreeNode* root) {
-        vector<vector<int>> ans;
-        if (!root)
-            return ans.size();
-        queue<TreeNode*> q;
-        q.push(root);
-
-        while (!q.empty()) {
-            int n = q.size();
-            vector<int> level;
-            for (int i = 0; i < n; i++) {
-                TreeNode* curr = q.front();
-                vector<int> level;
-                q.pop();
-
-                if (curr->left)
-                    q.push(curr->left);
-                if (curr->right)
-                    q.push(curr->right);
-                level.push_back(curr->val);
-            }
-            ans.push_back(level);
-        }
-        return ans.size();
+        return f(root);
     }
-
 };
