e5b094d8f52b727bd00f7aeda8c0a7e304d6c3b6
Divyam Choudhary <divyamchoudhary15@gmail.com>
2026-08-08 16:40:11 +0530

    Time: 10 ms (45.80%) | Memory: 7.7 MB (89.16%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -1,11 +1,7 @@
 class Solution {
 public:
     int fib(int n) {
-        if( n== 0 || n == 1) return n;
-
-        int l = fib(n-1);
-        int r = fib(n-2);
-
-        return l+r;
+        if(n == 0 || n == 1) return n;
+        return fib(n-1) + fib(n-2);
     }
 };
