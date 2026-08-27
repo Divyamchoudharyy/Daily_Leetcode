3ec328c311e7ba74179c0097475cf9b05ceb8307
Divyam Choudhary <divyamchoudhary15@gmail.com>
2026-08-27 15:42:13 +0530

    Time: 0 ms (100.00%) | Memory: 10.6 MB (65.14%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,23 +1,18 @@
 class Solution {
 private:
-    int f(int ind, vector<int>& nums , vector<int>& dp) {
-        if (ind < 0)
+    int f(int ind , vector<int>& nums,vector<int>& dp){
+        if(ind >= nums.size())
             return 0;
-        if (ind == 0)
-            return nums[0];
+        if(dp[ind]!=-1) return dp[ind];
+        int take = f(ind+2,nums,dp) + nums[ind];
+        int notake = f(ind+1,nums,dp);
 
-        if(dp[ind] != -1) return dp[ind];
-        int notake = f(ind - 1, nums,dp);
-
-        int take = nums[ind] + f(ind - 2, nums,dp);
-
-        return dp[ind] = max(take, notake);
+        return dp[ind] = max(take,notake);
     }
-
 public:
     int rob(vector<int>& nums) {
-        int n = nums.size();
-        vector<int> dp(n+1,-1);
-        return f(n - 1, nums,dp);
+        vector<int> dp(nums.size()+1,-1);
+        return f(0,nums,dp);
+
     }
 };
