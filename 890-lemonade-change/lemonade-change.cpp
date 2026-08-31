3a9d20636b8f00aecefef9bffd185499766c1548
Divyam Choudhary <divyamchoudhary15@gmail.com>
2026-09-01 00:44:16 +0530

    Time: 0 ms (100.00%) | Memory: 87.2 MB (75.91%) - LeetSync

---
+++ 890-lemonade-change/lemonade-change.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool lemonadeChange(vector<int>& bills) {
+        int c5 = 0 , c10 = 0;
+        for(int i = 0 ; i < bills.size();i++){
+            if(bills[i] == 5)
+                c5++;
+            else if(bills[i] == 10){
+                if(c5)
+                    c5-- , c10++;
+                else return false;
+            }else{
+                if(c5 && c10)
+                    c5-- , c10--;
+                else if(c5 > 2)
+                    c5-=3;
+                else return false;
+            }
+        }
+        return true;
+    }
+};
