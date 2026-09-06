03c26dbc9c054e03b85134014c3b6cc913422262
Divyam Choudhary <divyamchoudhary15@gmail.com>
2026-09-06 15:27:21 +0530

    Time: 540 ms (21.80%) | Memory: 296.6 MB (6.96%) - LeetSync

---
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -0,0 +1,21 @@
+class Solution {
+private:
+    int f(int ind , int prev , vector<int>& nums , vector<vector<int>>& dp){
+        if(ind == nums.size()) return 0;
+
+        if(dp[ind][prev+1] != -1) return dp[ind][prev+1];
+
+        int notake = f(ind+1,prev,nums,dp);
+        int take =0;
+        if(prev == -1 || nums[ind] > nums[prev])
+            take = 1 + f(ind + 1, ind,nums,dp);
+        
+        return dp[ind][prev+1] = max(take , notake);
+    }
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        vector<vector<int>> dp(nums.size()+1,vector<int>(nums.size()+1,-1));
+
+        return f(0,-1,nums,dp);
+    }
+};
